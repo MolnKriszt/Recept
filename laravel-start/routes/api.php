d0b1bd6e86d6e2e9522cedf92068cbaff1536cf5
Molnkriszt <krisz200601@gmail.com>
2025-02-10 14:57:52 +0100

    helo

--- laravel-start/routes/api.php
+++ laravel-start/routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+// Test
+Route::get('/', function(){
+    return 'API';
+});
