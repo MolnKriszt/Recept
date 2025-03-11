fa9f44db04ba6e8a38c85dd6b3229eeefb0387ba
Molnkriszt <krisz200601@gmail.com>
2025-03-11 14:12:11 +0100

    haladás

---
+++ Frontend/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <header>
+      <div>
+        <nav class="navbar bg-body-tertiary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              <img
+                src=""
+                alt="Logo"
+                width="30"
+                height="24"
+                class="d-inline-block align-text-top"
+              />
+              Receptek
+            </a>
+          </div>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style>
+.navbar{
+  background-color: var(--main-color) !important;
+}
+</style>
---
+++ Frontend/src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>hellos2</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
