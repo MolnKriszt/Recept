cd21cfea41ccc7ceafd8336a4e09bad1900359ca
MolnKriszt <krisz200601@gmail.com>
2025-03-12 22:12:00 +0100

    users login logout kész

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AuthenticateMy;
 
 Route::get('/', function(){
     return 'API';
 });
+
+
+//region users
+Route::post('users/login', [UsersController::class, 'login']);
+Route::post('users/logout', [UsersController::class, 'logout']);
+Route::get('users', [UsersController::class, 'index'])
+    ->middleware('auth:sanctum');
+Route::post('users', [UsersController::class, 'store'])
+    ->middleware('auth:sanctum');    
+Route::patch('users/{id}', [UsersController::class, 'update'])
+    ->middleware('auth:sanctum');    
+Route::delete('users/{id}', [UsersController::class, 'destroy'])
+    ->middleware('auth:sanctum');
+//endregion
